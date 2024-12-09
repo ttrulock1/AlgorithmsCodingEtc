d022ec0abb2bc6155ef3f4b816430b66e33de3f0
Todd Trulock <toddstrulock@gmail.com>
2024-12-09 11:49:48 -0700

    added ternary operators.

--- nodePaths/eloquentJavascriptRobot.js
+++ nodePaths/eloquentJavascriptRobot.js
@@ -38,7 +38,79 @@ function buildGraph(edges){
 const roadGraph = buildGraph(roads);
 
 
-class VillageState{
 
-    .move
+ 
+
+class VillageState{
+    constuctor(place,parcles){
+        this.place=place;
+        this.parcles=parcels
+    }
+
+    move(destination){
+        if(!roadGraph[this.place].includes()){
+        return this
+        }else{
+            let parcels = this.parcels.map(p => {
+                if(p.place != this.place) return p;
+                return {place: destination, address: p.address};
+            }).filter(p =>p.place != p.address);
+            return new VillageState(destination, parcels)
+        }
+    }
+}
+
+let first = new VillageState("Post Office", [{place: "Post Office", address: "Alice's House"}]);
+
+let next = first.move("Alice's House")
+
+//Data structures that don't change are called immutable or persistent. 
+// They behave like strings because they are who they.
+//however in Javascript most everything can be mutated.
+
+// Anything that makes your code easier to understand makes it possible to build a more ambitious system.
+
+//Simulation
+//Robot takes the VillageState and trys to come up with the best way to get the fucking packages.
+//To think like a coder we must think:
+//What must a robot do to solve this.
+
+function runRobot(package,robot,location)
+
+function runRobot(state, robot, memory){
+    for(let turn = 0;; turn++){
+        if(state.parcels.length==0){
+            console.log(`Done in ${turn} turns`)
+            break;
+        }
+        let action = robot(state, memory);
+        state = state.move(action.direction);
+        memory = action.memory;
+        console.log(`moved to $action.direction`)
+    }
+}
+
+function findRoute(graph, from, to){
+    let work = [{at: from, route:[]}];
+    for(let i = 0; i < work.length; i++){
+        let{at,route} = work[i];
+        for(let place of graph[at]){
+            if(place == to) return route.concat(place);
+            if(!work.some(w => w.at == place)){
+                work.push({at: place, route: route.concat(place)})
+            }
+        }
+    }
+}
+
+function goalOrientedRobot({place, parcels}, route){
+    if (route.length === 0){
+        let parcel = parcels[0];
+        if (parcel.place != place){
+            route= findRoute(roadGraph, place, parcel.place);
+        } else {
+            route = findRoute(roadGraph, place, parcel.add)
+        }
+    }
+    return {direction: route[0], memory: route.slice(1)};
 }
